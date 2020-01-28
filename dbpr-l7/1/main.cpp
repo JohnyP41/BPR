34f5f375911b2117dbe9abacae528101d35bdc3f
JohnyP41 <plaljahn@gmail.com>
2020-01-28 01:49:30 +0100

    Add files via upload

---
+++ dbpr-l7/1/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string login;
+    string haslo;
+    cout<<"Podaj login"<<endl;
+    cin>>login;
+    if(login=="eloelo320"){
+        cout<<"Podaj hasło"<<endl;
+        cin>>haslo;
+		if(haslo=="mwwwwwwwwwwwww11111111121qq"){
+			cout<<"Witaj użytkowniku"<<endl;
+                //obsługa użytkownika
+  		}
+		else{
+			cout<<"Niepoprawne hasło"<<endl;
+		}     
+    }else
+    {
+        cout<<"Niepoprawny login"<<endl;
+    }
+}
